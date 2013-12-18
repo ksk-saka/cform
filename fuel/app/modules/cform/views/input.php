dec2e900873b1ce9538a77a7868e257b2ea171b7
Keisuke Sakamoto <ksk.skmt@gmail.com>
2013-12-19 00:11:58 +0900

    fix #5

--- fuel/app/modules/cform/views/input.php
+++ fuel/app/modules/cform/views/input.php
@@ -1,1 +1,17 @@
-いんぷっと。
+<form action="<?php echo Uri::base(); ?>confirm" method="post">
+<p>
+	名前：<br>
+	<input type="text" name="name" value="">
+</p>
+<p>
+	メールアドレス：<br>
+	<input type="text" name="email" value="">
+</p>
+<p>
+	内容：<br>
+	<textarea name="memo" rows="6" cols="70"></textarea>
+</p>
+<div>
+	<input type="submit" name="submit" value="確認">
+</div>
+</form>
