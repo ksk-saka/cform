7d2a88f25bdf78985b4548e3233cd27f74a4400a
Keisuke Sakamoto <ksk.skmt@gmail.com>
2013-12-29 13:22:26 +0900

    refs #12

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -6,5 +6,16 @@
  */
 
 return array(
-
+	'active' => 'default',
+	'default' => array(
+		'type'        => 'mysqli',
+		'connection'  => array(
+			'persistent' => false,
+		),
+		'identifier'   => '`',
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'enable_cache' => true,
+		'profiling'    => false,
+	),
 );
